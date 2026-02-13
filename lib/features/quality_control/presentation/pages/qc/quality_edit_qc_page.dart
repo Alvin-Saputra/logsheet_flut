d1ed1922593487ae122b44cffdbcfbcfd4ec5664
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2026-02-13 15:13:36 +0700

    Updating the interlock filtering in F/QCO 02 to the new schema of dialy production refinery R/RFA 004A

--- lib/features/quality_control/presentation/pages/qc/quality_edit_qc_page.dart
+++ lib/features/quality_control/presentation/pages/qc/quality_edit_qc_page.dart
@@ -1389,7 +1389,7 @@ class _QualityEditQCPageState extends State<QualityEditQCPage> {
                                                   }).toList(),
                                               onChanged: (value) {
                                                 setState(() {
-                                                  selectedWorkCenter = value;
+                                                 selectedWorkCenterDailyProductionRefinery = value;
                                                 });
                                               },
                                               decoration: InputDecoration(
@@ -1425,48 +1425,48 @@ class _QualityEditQCPageState extends State<QualityEditQCPage> {
                                           icon: Icons.event,
                                         ),
                                         SizedBox(height: 12),
-                                        DropdownButtonFormField<String>(
-                                          value: selectedShift,
-                                          items:
-                                              dummyShiftOptions.map((item) {
-                                                return DropdownMenuItem<String>(
-                                                  value: item,
-                                                  child: Text(
-                                                    "${item}",
-                                                    style: const TextStyle(
-                                                      fontSize: 14,
-                                                    ),
-                                                  ),
-                                                );
-                                              }).toList(),
-                                          onChanged: (value) {
-                                            setState(() {
-                                              selectedShift = value;
-                                            });
-                                          },
-                                          decoration: InputDecoration(
-                                            filled: true,
-                                            fillColor: const Color(0xFFF0ECE9),
-                                            border: OutlineInputBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(12),
-                                              borderSide: BorderSide.none,
-                                            ),
-                                            labelText: 'Pilih Shift',
-                                            floatingLabelBehavior:
-                                                FloatingLabelBehavior.auto,
-                                            prefixIcon: Padding(
-                                              padding: const EdgeInsets.all(
-                                                12.0,
-                                              ),
-                                              child: SvgPicture.asset(
-                                                'assets/icons/oil-refinery-tanks.svg',
-                                                height: 24,
-                                                width: 24,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
+                                        // DropdownButtonFormField<String>(
+                                        //   value: selectedShift,
+                                        //   items:
+                                        //       dummyShiftOptions.map((item) {
+                                        //         return DropdownMenuItem<String>(
+                                        //           value: item,
+                                        //           child: Text(
+                                        //             "${item}",
+                                        //             style: const TextStyle(
+                                        //               fontSize: 14,
+                                        //             ),
+                                        //           ),
+                                        //         );
+                                        //       }).toList(),
+                                        //   onChanged: (value) {
+                                        //     setState(() {
+                                        //       selectedShift = value;
+                                        //     });
+                                        //   },
+                                        //   decoration: InputDecoration(
+                                        //     filled: true,
+                                        //     fillColor: const Color(0xFFF0ECE9),
+                                        //     border: OutlineInputBorder(
+                                        //       borderRadius:
+                                        //           BorderRadius.circular(12),
+                                        //       borderSide: BorderSide.none,
+                                        //     ),
+                                        //     labelText: 'Pilih Shift',
+                                        //     floatingLabelBehavior:
+                                        //         FloatingLabelBehavior.auto,
+                                        //     prefixIcon: Padding(
+                                        //       padding: const EdgeInsets.all(
+                                        //         12.0,
+                                        //       ),
+                                        //       child: SvgPicture.asset(
+                                        //         'assets/icons/oil-refinery-tanks.svg',
+                                        //         height: 24,
+                                        //         width: 24,
+                                        //       ),
+                                        //     ),
+                                        //   ),
+                                        // ),
                                         SizedBox(height: 12),
                                         Row(
                                           mainAxisSize: MainAxisSize.max,
@@ -1523,9 +1523,9 @@ class _QualityEditQCPageState extends State<QualityEditQCPage> {
                                                                   true,
                                                             ),
                                                             plantCode,
-                                                            parseInt(
-                                                              selectedShift,
-                                                            ),
+                                                            // parseInt(
+                                                            //   selectedShift,
+                                                            // ),
                                                             selectedWorkCenterDailyProductionRefinery,
                                                           );
                                                           log(
